19af290322187dbe1654730e1f8b18850edd34ef
morrita <you@example.com>
2017-02-20 23:10:04 +0000

    update via update_git.sh

--- cammy_lib.py
+++ cammy_lib.py
@@ -558,3 +558,43 @@ def access_keepalive(verbose,keepalive_file,keepalive_action,tidy_list, logfile,
     else:
         pass
 
+
+
+# decorator function
+# parameter list (in order):
+# trace_level = the level of debug information to be logged (0 = none)
+# log_file = the filename (including path) to where log entries get written
+
+def logging_decorator (trace_level,log_file):
+    def real_decorator(f):
+
+        from functools import wraps
+        from datetime import datetime
+
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+
+            if trace_level > 0:
+                datestr = get_date()
+                update_file ("INFO: trace_level = %s, log entry made before function %s was called at: %s\n" % (str(trace_level),f.__name__,datestr),log_file)
+
+            if trace_level > 1:
+                t1 = datetime.now()
+
+            r = f(*args, **kwargs)      # call the original function
+
+            if trace_level > 1:
+                t2 = datetime.now()
+                elapsed_time = str((t2 - t1).total_seconds())
+                update_file ("INFO: trace_level = %s, the time taken to execute function %s was %s seconds\n" % (str(trace_level),f.__name__,elapsed_time),log_file)
+
+            if trace_level > 0:
+                datestr = get_date()
+                update_file ("INFO: trace_level = %s, log entry made after function %s was called at: %s\n" % (str(trace_level),f.__name__,datestr),log_file)
+
+            return r
+        return wrapped
+    return real_decorator
+
+
+
